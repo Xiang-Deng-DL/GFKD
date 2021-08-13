a701fadc5d441020519132f12d0f82a2eaa5d7ca
Xiang Deng <41453967+Xiang-Deng-DL@users.noreply.github.com>
2021-08-13 13:07:29 -0400

    Rename generate_fake_GCN.sh to generate_fake_graphs.sh

--- generate_fake_GCN.sh
+++
@@ -1,1 +0,0 @@
-python inversion_gcn.py --gpu 0 --trial 1 --epoch 1500 --self_loop --bn_reg_scale 1e-2 --onehot_cof 1e-6 --dataset MUTAG --savepath './save/fakegraphs' --tmodel GCN --modelt GCN5_64 --path_t './save/teachers/Model:GCN5_64_Datset:MUTAG'
